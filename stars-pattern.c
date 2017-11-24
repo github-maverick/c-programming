14a755d2a510aaf7057b28a315f478a4bddc046b
Maverick <rabin.adk1@gmail.com>
2017-11-24 12:41:21 +0545

    Decreased code

--- stars-pattern.c
+++ stars-pattern.c
@@ -2,10 +2,8 @@
 int main() {
 	int x, y;
 	for (x=1 ; x<=10 ; x++ ) {
-		for (y=1; y<=x ; y++ ) {
+		for (y=1; y<=x ; y++ )
 			printf("*");
-			if (x==y)
-				printf("\n");
-		}
+		printf("\n");
 	}
 }
